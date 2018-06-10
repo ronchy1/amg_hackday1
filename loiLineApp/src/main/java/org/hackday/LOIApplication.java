34ab8ee56e57902ae2cfeb9113309094260f00ad
ronchy.yuen <ronchy@163.com>
2018-06-10 14:33:22 +0800

    reload

---
+++ loiLineApp/src/main/java/org/hackday/LOIApplication.java
@@ -0,0 +1,14 @@
+package org.hackday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class LOIApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(LOIApplication.class);
+    }
+}
